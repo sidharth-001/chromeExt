ac93b747111e02ae6438a2129eafcdc79245cb7f
Sidharth <sidnagar08@gmail.com>
2020-09-27 14:59:48 +0530

    v2.0

--- content.js
+++ content.js
@@ -1,19 +1,10 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === "get_images") {
-        let formatted_images = [];
-
-        let images = $('img').map((i, img) => {
-            if($(img).data('src')) {
-                formatted_images.push({
-                    src: $(img).data('src')
-                });
-            } else if($(img).src) {
-                formatted_images.push({
-                    src: $(img).src
-                });
-            }
-        });
-
+    if (request.action === "get_images"){
+        var imgs = document.getElementsByTagName("img");
+        var formatted_images = [];
+        for (let i = 0; i < imgs.length; i++){
+            formatted_images.push(imgs[i].src);
+        }
         sendResponse(formatted_images);
     }
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,19 +1,33 @@
 var images = [];
+var imageNum = 0;
 
 chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {action: "get_images"}, response => {
         $('.gallery').html('');
-
         images = response;
         response.map((img) => {
-            $('.gallery').append('<img src="' + img.src + '" />');
+            if(img) imageNum++;
+        });
+        $('.gallery').append('<h3>Number of images: '+ imageNum +'</h3>');
+        response.map((img) => {
+            var image = new Image();
+            image.src = img;
+            document.body.appendChild(image);
         });
     });
 });
 
+
 $(document).on('click', '#download_all', (e) => {
-    console.log("Downloading all");
-    chrome.runtime.sendMessage({action: "download", data: images}, res => {
-        console.log("Complete");
-    });
+    let i=0;
+    setInterval(function(){
+        if(images[i]){
+            var link = document.createElement("a");
+            link.id=i;
+            link.download = "images.png";
+            link.href = images[i];
+            link.click();
+            i++;
+        }
+    },100);
 });
